ca402e9281f3ea7b5d24bc042a1bb687890c3fd5
Mithras Kuipers <mithraskuipers@gmail.com>
2022-08-25 15:32:41 +0200

    env manipulation added

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 21:20:37 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/08/25 15:30:34 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/08/25 15:32:39 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(int argc, char **argv, char **envp)
 	t_ms *ms;
 	ms = ft_calloc(1 + 1, sizeof(t_ms));
 
-	init_wrapper(ms, envp);
+	if (init_wrapper(ms, envp))
+		return (1);
 	env_add_entry(ms, "SHELL", "42");
 	env_add_entry(ms, "Thiswillbeaddedtothebottom", "See?");
 	dbg_print_env(ms);
