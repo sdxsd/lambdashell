f8110d0955f1d8e2c9de587614b1966684a33b58
Mithras Kuipers <mithraskuipers@gmail.com>
2022-08-25 15:30:37 +0200

    update

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/22 12:24:35 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/08/25 14:58:54 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/08/25 15:28:01 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int	init_ms(t_ms *ms)
 	ms = ft_calloc(1, sizeof(t_ms));
 	if (!ms)
 	{
-		printf("Error: Mem allocation for ms struct failed.\n");
-		return (1);
+		return (msg_err("Mem allocation for ms struct failed.", 1));
 	}
 	return (0);
 }
@@ -30,14 +29,9 @@ int	init_env(t_ms *ms, char **envp)
 	i = 0;
 	while (envp[i])
 		i++;
-	//ms->env = ft_calloc((i + 1), (sizeof(char *)));
-	ms->env = malloc((1) * sizeof(char *));
+	ms->env = ft_calloc((i + 1), (sizeof(char *)));
 	if (!ms->env)
-	{
-		printf("Error: Mem allocation for environment failed.\n");
-		clean_ms_wrapper(ms);
-		return (1);
-	}
+		return (msg_err("Mem allocation for environment failed.", 1));
 	i = 0;
 	while (envp[i])
 	{
@@ -109,53 +103,10 @@ int	env_add_entry(t_ms *ms, char *key, char *value)
 int	init_wrapper(t_ms *ms, char **envp)
 {
 	if (!isatty(0))
-		return(msg_err ("Standard input fd does not refer to terminal.", 1));
+		return(msg_err("Standard input fd does not refer to terminal.", 1));
 	if (init_ms(ms))
-		return(msg_err ("Failed to initialize ms.", 1));
+		return(msg_err("Failed to initialize ms.", 1));
 	if (init_env(ms, envp))
-		return(msg_err ("Failed to initialize env.", 1));
+		return(msg_err("Failed to initialize env.", 1));
 	return (0);
 }
-
-/*
-env_write_entry(3)
-Serves as a helper function for env_add_entry(3). It will add a new 
-user-specified entry to env_global at the specified position.
-*/
-
-/*
-env_find_key(1)
-Searches in env for the provided key. If found, it returns the position
-of the key. Else, returns -1.
-*/
-
-/*
-env_add_entry(2)
-Adds an entry to env. It expects that the new entry is provided in 
-key-value format. It uses env_find_key(2) to decide where to write the entry
-in env. If env_find_key(2) could not find the key in env, it will
-tell env_add_entry(3) to add the entry to the end of env. If the key
-already exists in env, it will replace its corresponding value with
-what is user-specified. It uses env_write_entry(3) for actually writing the 
-entry.
-*/
-
-/*
-init()
-- Checks whether the standard input file descriptor refers to a terminal.
-- Calls init_env() to initialize our global variable env_global.
-- TODO: Shorten by combining all if statements on single line
-*/
-
-/*
-get_shell_lvl()
--
--
-*/
-
-/*
-int	get_shell_lvl(char **envp)
-{
-
-}
-*/
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 21:20:37 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/08/25 14:57:17 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/08/25 15:30:34 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_ms *ms;
-	ms = ft_calloc(1, sizeof(t_ms));
+	ms = ft_calloc(1 + 1, sizeof(t_ms));
 
 	init_wrapper(ms, envp);
-	//dbg_print_env(ms);
+	env_add_entry(ms, "SHELL", "42");
+	env_add_entry(ms, "Thiswillbeaddedtothebottom", "See?");
+	dbg_print_env(ms);
+	//clean_ms_wrapper(ms);
 	(void)argc;
 	(void)argv;
-	clean_ms_wrapper(ms);
 	return 0;
 }
