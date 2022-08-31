c0e02f7f73c97c8c754032e3bd70e5a8db81887e
Mithras Kuipers <mithraskuipers@gmail.com>
2022-08-31 20:18:15 +0200

    added env tools

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/25 14:38:27 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/08/31 18:22:38 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/08/31 20:14:37 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,18 @@ int	dbg_print_env(t_ms *ms)
 		cur = cur->next;
 	}
 	printf("%s%s\n", cur->key, cur->val);
+	return (1);
+}
+
+int	dbg_print_env_idx(t_ms *ms)
+{
+	t_env *cur;
+	cur = *ms->env;
+	while (cur->next)
+	{
+		printf("[%d] %s%s\n", cur->idx, cur->key, cur->val);
+		cur = cur->next;
+	}
+	printf("[%d] %s%s\n", cur->idx, cur->key, cur->val);
 	return (1);
 }
