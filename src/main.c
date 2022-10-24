130e8910b8351e65210255bb0ad74850ceb43c4d
sdxsd <willmaguire@icloud.com>
2022-10-24 10:55:21 +0200

    More deletion.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int	prompt(t_ms *ms)
 	ms->line = readline("λ :: > ");
 	if (ft_strlen(ms->line) < 1)
 		return (SUCCESS);
-	line_parser(ms);
+	parse_line(ms);
 	exec_list = tokenizer(ms);
 	exec_list_generator(exec_list, ms->env);
 	dbg_print_exec_list(exec_list);
@@ -22,22 +22,8 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_ms *ms;
 
-	splash();
 	if (argc == 1 && argv)
-	{
-		if (check_fd())
-			return (msg_err("main()", FAILURE));
-		ms = ft_calloc(1, sizeof(t_ms));
-		if (!ms)
-			return (msg_err("main()", FAILURE));
-		if (init_ms_struct(ms))
-			return (msg_err("main()", FAILURE));
-		if (init_env_struct(ms, envp))
-			return (msg_err("main()", FAILURE));
 		while (TRUE)
-		{
 			prompt(ms);
-		}
-	}
-	return (FAILURE);
+	return (SUCCESS);
 }
