1a035049e4765983d7e5308afa3755240b6ed223
sdxsd <willmaguire@icloud.com>
2022-10-28 15:44:09 +0200

    Cosmetic functions.

--- src/cosmetic/cosmetic.c
+++ src/cosmetic/cosmetic.c
@@ -37,63 +37,9 @@ The definition of Free Software is as follows:
 A program is free software if users have all of these freedoms.
 */
 
-#ifndef PROTOTYPES_H
-# define PROTOTYPES_H
-# include "structs.h"
+#include "../../include/minishell.h"
 
-/* COSMETIC */
-void	clear(void);
-void	red(void);
-void	green(void);
-void	blue(void);
+/* void	ps1(t_shell	*lambda) */
+/* { */
 
-/* BUILTINS */
-char			*ret_cwd(void);
-int				pwd(void);
-void			cd(t_cmd *cmd, t_vector *env);
-t_cmd			*bltin_constructor(char	*line, t_vector *env);
-
-/* SHELL */
-int				prompt(t_shell *ms);
-
-/* ENVIRONMENT */
-t_vector		*init_env(char **env);
-char			**env_to_strings(t_vector *env);
-t_env_element	*env_get_val(t_vector *env, char *val);
-
-/* ERROR MESSAGES */
-int				msg_err(char *s, int ret);
-void			*null_msg_err(char *s);
-
-/* TOKENISATION */
-t_exec_element	*tokenizer(t_shell *shell);
-
-/* EXECUTION */
-int				execute_command(t_cmd *cmd);
-int				executor(t_exec_element *head, t_vector *env);
-void			dealloc_exec_list(t_exec_element *head);
-
-/* EXEC LIST */
-t_exec_element	*exec_list_generator(t_exec_element *head, t_vector *env);
-t_exec_element	*new_exec_element(void);
-void			free_exec_list(t_exec_element *head);
-
-/* LINE PARSER */
-char			**parse_line(t_shell *lambda);
-
-/* CMD STRUCT */
-t_cmd			*cmd_constructor(char *prog_n, t_vector *env);
-void			cmd_deallocator(t_cmd *cmd);
-
-/* PATH */
-char			*get_path(char *prog_n, t_vector *env);
-int				free_ptr_array(char *ptr[]);
-
-/* DEBUG */
-void			dbg_print_exec_list(t_exec_element *head);
-void			dbg_print_lines(char **lines);
-void			dbg_print_env(t_vector *head);
-void			dbg_print_cmd(t_cmd	*cmd);
-void			dbg_test_env(t_vector *env);
-
-#endif
+/* } */
