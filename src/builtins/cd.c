da9fa10843873ee4ff662cef9a7b8d46a0d59c0b
MyNameIsTrez <sbos@student.codam.nl>
2022-11-14 13:53:08 +0100

    PWD begone

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -41,13 +41,8 @@ A program is free software if users have all of these freedoms.
 #include <unistd.h>
 #include <stdlib.h>
 
-void	cd(t_cmd *cmd, t_vector *env)
+void	cd(t_cmd *cmd)
 {
 	if (chdir(cmd->args[1]))
 		msg_err("cd", FAILURE);
-	else
-	{
-		free(env_get_val(env, "PWD=")->val);
-		env_get_val(env, "PWD=")->val = ret_cwd();
-	}
 }
