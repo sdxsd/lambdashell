e5451c43191b27c4541d58d080b66bc356bf9211
MyNameIsTrez <sbos@student.codam.nl>
2023-01-11 17:46:48 +0100

    Add perror_error()

--- include/prototypes.h
+++ include/prototypes.h
@@ -91,6 +91,7 @@ bool			is_valid_identifier(char *name);
 t_status		print_error(char *msg);
 t_status		prefixed_error(char *msg);
 t_status		prefixed_perror(char *msg);
+t_status		perror_error(char *msg);
 
 /* EXEC */
 t_status		execute(t_shell *lambda);
